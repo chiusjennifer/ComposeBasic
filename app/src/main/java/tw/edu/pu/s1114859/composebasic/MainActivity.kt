37e8f4e3bb76190616e05d1bef6efeef33acc71f
chiusjennifer <chiusjennifer@gmail.com>
2024-04-17 18:02:35 +0800

    0417 18:02 commit

--- app/src/main/java/tw/edu/pu/s1114859/composebasic/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1114859/composebasic/MainActivity.kt
@@ -3,6 +3,7 @@ package tw.edu.pu.s1114859.composebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -10,12 +11,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,7 +36,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import tw.edu.pu.s1114859.composebasic.ui.theme.ComposeBasicTheme
 
@@ -49,7 +59,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var msg by remember { mutableStateOf("行動應用軟體開發")}
+    var count = remember { mutableStateOf(0) }
+    var password by remember { mutableStateOf("") }
+
     Column {
+        TextField(
+            value = msg,
+            onValueChange = { newText ->
+                msg = newText
+            },
+            label = { Text(text = "訊息") },
+            placeholder = { Text(text = "請輸入您的訊息") },
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Number)
+
+        )
+        TextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = { Text(text = "請輸入密碼") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Password)
+        )
+
+
+
         Row {
             Text(
                 text = "$name ",
@@ -68,15 +106,44 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
             )
 
-        }
-        Button(onClick = {},
-            colors = buttonColors(Color.DarkGray)
 
-        ) {
-            Text(
-                "按鈕",
-                color = Color.White
+        }
+        Button(onClick = {
+            if (msg == "行動應用軟體開發"){
+                msg = "Android App"
+                count.value=0
+            }
+            else{
+                msg = "行動應用軟體開發"
+            }
+
+        },
+            colors = buttonColors(Color.DarkGray),
+            shape= RoundedCornerShape(20.dp),
+            //shape= CutCornerShape(10)
+            border= BorderStroke(1.dp, Color.Blue),
+            elevation =  ButtonDefaults.buttonElevation(
+                defaultElevation = 10.dp,
+                pressedElevation = 15.dp,
+                disabledElevation = 0.dp
+
             )
+        ){
+            Image(
+                painterResource(id = R.drawable.animal0),
+                contentDescription ="黃色小鴨",
+                modifier = Modifier.size(20.dp)
+            )
+
+            Text(
+                text = "按鈕 ",
+                color = Color.Magenta
+            )
+            Text(
+                text = msg,
+                color = Color.Green
+            )
+
 
         }
         Box (
@@ -84,7 +151,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             contentAlignment= Alignment.Center
         ){
             //var count by remember { mutableStateOf(0) }
-            var count = remember { mutableStateOf(0) }
+
 
             Text(
             text = count.value.toString(),
